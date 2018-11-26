4794b86bc18bf7663999e4c6f8069186ae038896
Gtm <35806300+gtming0508@users.noreply.github.com>
2018-11-26 09:56:19 +0800

    gg

---
+++ aa.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class componentName extends Component {
+  render() {
+    return (
+      <div>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>9</div>
+        <div>5</div>
+        <div>1</div>
+      </div>
+    )
+  }
+}
