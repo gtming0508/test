43adcdb894882b8a867cb2c0fcc6cdb46e4f824d
gaotianming <gaotianming@forex.com.cn>
2018-12-05 16:52:54 +0800

    123

--- aa.jsx
+++ aa.jsx
@@ -4,7 +4,7 @@ export default class componentName extends Component {
   render() {
     return (
       <div>
-        <div>1</div>
+        <div>123</div>
         <div>2</div>
         <div>3</div>
         <div>4</div>
